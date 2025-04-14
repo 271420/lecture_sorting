ef6a4bcc6b4b8aafbf739e7c600f87ee6fef46b3
Eliska Uncovska <271420@vutbr.cz>
2025-04-14 12:37:20 +0200

    update

--- sorting.py
+++ sorting.py
@@ -1,4 +1,7 @@
 import os
+import csv
+
+from Tools.scripts.generate_opcode_h import header
 
 
 def read_data(file_name):
@@ -10,9 +13,20 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
-
+    with open (file_path,"r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {}
+        for row in reader:
+            for header, value in row.items():
+                if header not in data:
+                    data[header] = [int(value)]
+                else:
+                    data[header].append(int(value))
+    return data
 
 def main():
+    data = read_data("numbers.csv")
+    print(data)
     pass
 
 
